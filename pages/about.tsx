5ddf969f1e084f5e0fbe7d9c74ac990f85dbbb34
raymondkdev <raymondkdev@gmail.com>
2021-07-20 17:36:10 +0200

    Added an 'about us' section

---
+++ pages/about.tsx
@@ -0,0 +1,19 @@
+import Head from "next/head";
+import AboutUs from "@/components/about/AboutUs";
+
+export const About = () => {
+  return (
+    <main>
+      <Head>
+        <title>About ProtonMail</title>
+        <meta
+          name="description"
+          content="We're building encrypted email that protects privacy. Meet our team and learn more about our mission to provide free secure email to all."
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <AboutUs />
+    </main>
+  );
+};
+export default About;
