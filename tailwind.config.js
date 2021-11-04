9014bd53dabf3fa9d228511b12427dd489f5eda5
raymondkdev <raymondkdev@gmail.com>
2021-11-04 17:50:49 +0100

    Added configuration for TailwindCSS

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  mode: "jit",
+  content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+};
