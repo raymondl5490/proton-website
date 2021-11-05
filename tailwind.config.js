c245837d891b430607bbd492cc8c76283e2c28ff
raymondkdev <raymondkdev@gmail.com>
2021-11-05 23:29:06 +0100

    Hero: Using TailwindCSS. Extended TailwindCSS' `backgroundImage` so we can use `@apply bg-mountains`. https://tailwindcss.com/docs/background-image

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@ module.exports = {
   mode: "jit",
   content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'mountains': "url('/home/mountains.png')",
+      }
+    },
   },
   variants: {
     extend: {},
