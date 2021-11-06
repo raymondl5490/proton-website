2eb554809d5b27208037a42a3c8a660b063c5aef
raymondkdev <raymondkdev@gmail.com>
2021-11-06 18:46:52 +0100

    Footer: Using TailwindCSS.

--- components/site/Footer/Logos/Logos.tsx
+++ components/site/Footer/Logos/Logos.tsx
@@ -4,10 +4,10 @@ import styles from "./Logos.module.scss";
 export const Logos = () => {
   return (
     <div className={styles.container}>
-      <div className={styles.logo}>
+      <div>
         <Image src="/swiss.svg" alt="Logo 1" width={165} height={41} />
       </div>
-      <div className={styles.logo}>
+      <div>
         <Image src="/logo-gdpr-eu-white.svg" alt="Logo 2" width={165} height={41} />
       </div>
     </div>
