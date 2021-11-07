6b05f08f5a2c0cbf648316c137292bc4a05cdc6f
raymondkdev <raymondkdev@gmail.com>
2021-11-07 14:39:25 +0100

    ContactInformation: Made padding consistent.

--- components/home/ContactInformation/ContactInformation.tsx
+++ components/home/ContactInformation/ContactInformation.tsx
@@ -9,58 +9,63 @@ library.add(faEnvelope);
 export const ContactInformation = () => {
   return (
     <div className={styles.container}>
-      <div className={styles.physicalAddress}>
-        <address className={`vcard ${styles.vcard}`}>
-          <span className={`fn org ${styles.org}`}>Proton Technologies AG</span>
-          <span className={`adr ${styles.adr}`}>
-            <span className="street-address">Route de la Galaise 32</span>
-            {", "}
-            <br />
-            <span className="postal-code">1228</span>{" "}
-            <span className="locality">Plan-les-Ouates</span>
-            <br />
-            <span className="region">Geneva</span>
-            {", "}
-            <span className="country-name">Switzerland</span>
-          </span>
-        </address>
-      </div>
-      <div className={styles.contactInformation}>
-        <p className={styles.ciText}>
-          <FontAwesomeIcon icon={faEnvelope} className={styles.icon} /> General:{" "}
-          <span title="contact@protonmail.com">Show email</span>
-        </p>
-        <p className={styles.ciText}>
-          <FontAwesomeIcon icon={faEnvelope} className={styles.icon} /> Media: media@protonmail.com
-        </p>
-        <p className={styles.ciText}>
-          <FontAwesomeIcon icon={faEnvelope} className={styles.icon} /> Legal: legal@protonmail.com
-        </p>
-        <p className={styles.ciText}>
-          <FontAwesomeIcon icon={faEnvelope} className={styles.icon} /> Partnership:
-          partners@protonmail.com
-        </p>
-      </div>
+      <div className={styles.inner}>
+        <div className={styles.physicalAddress}>
+          <address className={`vcard ${styles.vcard}`}>
+            <span className={`fn org ${styles.org}`}>Proton Technologies AG</span>
+            <span className={`adr ${styles.adr}`}>
+              <span className="street-address">Route de la Galaise 32</span>
+              {", "}
+              <br />
+              <span className="postal-code">1228</span>{" "}
+              <span className="locality">Plan-les-Ouates</span>
+              <br />
+              <span className="region">Geneva</span>
+              {", "}
+              <span className="country-name">Switzerland</span>
+            </span>
+          </address>
+        </div>
+        <div className={styles.contactInformation}>
+          <p className={styles.ciText}>
+            <FontAwesomeIcon icon={faEnvelope} className={styles.icon} /> General:{" "}
+            <span title="contact@protonmail.com">Show email</span>
+          </p>
+          <p className={styles.ciText}>
+            <FontAwesomeIcon icon={faEnvelope} className={styles.icon} /> Media:
+            media@protonmail.com
+          </p>
+          <p className={styles.ciText}>
+            <FontAwesomeIcon icon={faEnvelope} className={styles.icon} /> Legal:
+            legal@protonmail.com
+          </p>
+          <p className={styles.ciText}>
+            <FontAwesomeIcon icon={faEnvelope} className={styles.icon} /> Partnership:
+            partners@protonmail.com
+          </p>
+        </div>
 
-      <div className={styles.contactInformation}>
-        <p className={styles.ciText}>
-          <FontAwesomeIcon icon={faEnvelope} className={styles.icon} /> Abuse: abuse@protonmail.com
-        </p>
-        <p className={styles.ciText}>
-          For legal/police inquiries,{" "}
-          <Link href="/law-enforcement">
-            <a>click here</a>
-          </Link>
-        </p>
-      </div>
+        <div className={styles.contactInformation}>
+          <p className={styles.ciText}>
+            <FontAwesomeIcon icon={faEnvelope} className={styles.icon} /> Abuse:
+            abuse@protonmail.com
+          </p>
+          <p className={styles.ciText}>
+            For legal/police inquiries,{" "}
+            <Link href="/law-enforcement">
+              <a>click here</a>
+            </Link>
+          </p>
+        </div>
 
-      <div className={styles.contactInformation}>
-        <p className={styles.ciText}>
-          For support inquiries, please visit{" "}
-          <Link href="https://protonmail.com/support">
-            <a>protonmail.com/support</a>
-          </Link>
-        </p>
+        <div className={styles.contactInformation}>
+          <p className={styles.ciText}>
+            For support inquiries, please visit{" "}
+            <Link href="https://protonmail.com/support">
+              <a>protonmail.com/support</a>
+            </Link>
+          </p>
+        </div>
       </div>
     </div>
   );
