fdfc3e27474ff13b95943616e00916521851132b
raymondkdev <raymondkdev@gmail.com>
2022-06-05 18:13:50 +0200

    Added TailwindCSS v3.0.24

--- app.vue
+++ app.vue
@@ -1,5 +1,28 @@
 <template>
-  <div>
-    <NuxtWelcome />
+  <div id="app" class="message">
+    <h1 class="hoverable">Hello World</h1>
+    <p class="text-sm">If this message is centered, Nuxt 3 with TailwindCSS is set up correctly.</p>
   </div>
 </template>
+
+<style lang="postcss">
+  @tailwind base;
+  @tailwind components;
+  @tailwind utilities;
+
+  #app {
+    @apply h-screen text-black;
+    @apply dark:bg-slate-900 dark:text-gray-300;
+  }
+
+  .message {
+    @apply flex flex-col justify-center items-center;
+    h1 {
+      @apply text-4xl mb-4;
+    }
+    .hoverable {
+      @apply transition-colors ease-in-out hover:text-blue-600;
+      @apply dark:hover:text-yellow-300;
+    }
+  }
+</style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,16 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-
-})
+  build: {
+    postcss: {
+      postcssOptions: {
+        plugins: {
+            'postcss-import': {},
+            'tailwindcss/nesting': {},
+          tailwindcss: {},
+          autoprefixer: {},
+        },
+      },
+    },
+  },
+});
